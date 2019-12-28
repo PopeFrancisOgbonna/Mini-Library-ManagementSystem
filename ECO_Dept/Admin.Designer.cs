45a81b7633bf5d980a69127bd968e0afee0cc047
PopeFrancisOgbonna <francisogbonna24@gmail.com>
2019-12-28 10:35:18 +0100

    Worked on the database design and connection of Admin module

--- ECO_Dept/Admin.Designer.cs
+++ ECO_Dept/Admin.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Admin));
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblEdit = new System.Windows.Forms.Label();
@@ -93,12 +96,37 @@
             // 
             // dataGridView1
             // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(417, 14);
             this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.dataGridView1.Size = new System.Drawing.Size(527, 321);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // btnSave
             // 
@@ -240,6 +268,7 @@
             this.btnDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnDelete.UseVisualStyleBackColor = false;
             this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -256,6 +285,7 @@
             this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnUpdate.UseVisualStyleBackColor = false;
             this.btnUpdate.Visible = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // Admin
             // 
--- ECO_Dept/Admin.cs
+++ ECO_Dept/Admin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace ECO_Dept
 {
@@ -17,6 +18,8 @@ namespace ECO_Dept
             InitializeComponent();
         }
         int count; //for switching the edit user role modules
+        //Database connection properties
+        private string connectionString = @"Database=.;Initial catalog=AirForce_Library;Integrated Security =true";
         private void lblEdit_Click(object sender, EventArgs e)
         {
            
@@ -29,6 +32,7 @@ namespace ECO_Dept
             }
             else{
                 dataGridView1.Visible = true;
+                loadData();
                 btnDelete.Visible = true;
                 btnUpdate.Visible = true;
                 count += 1;
@@ -37,7 +41,10 @@ namespace ECO_Dept
 
         private void txtService_TextChanged(object sender, EventArgs e)
         {
-            if(txtService.Text != "") { btnClear.Visible = true; }
+            if(txtService.Text != "")
+            {
+                btnClear.Visible = true;
+            }
         }
 
         private void txtRole_TextChanged(object sender, EventArgs e)
@@ -52,6 +59,8 @@ namespace ECO_Dept
             txtRank.Clear();
             txtRole.Clear();
             txtService.Clear();
+            btnDelete.Visible = false;
+            btnUpdate.Visible = false;
         }
 
         private void btnSave_Click(object sender, EventArgs e)
@@ -62,7 +71,129 @@ namespace ECO_Dept
             }
             else
             {
-                MessageBox.Show("confirmed!");
+                using (SqlConnection connect = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        string pass = "12345";
+                        string query = "insert into users values(@param,@param1,@param2,@param3,@param4,@param5,@param6)";
+                        SqlCommand command = new SqlCommand(query, connect);
+                        command.Parameters.AddWithValue("@param", txtService.Text);
+                        command.Parameters.AddWithValue("@param1", txtRank.Text);
+                        command.Parameters.AddWithValue("@param2", txtName.Text);
+                        command.Parameters.AddWithValue("@param3", txtPost.Text);
+                        command.Parameters.AddWithValue("@param4", txtRole.Text);
+                        command.Parameters.AddWithValue("@param5", txtName.Text);
+                        command.Parameters.AddWithValue("@param6", pass);
+
+                        connect.Open();
+                        int i = command.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            MessageBox.Show("Record Saved Successfully");
+                            loadData();
+                            dataGridView1.Visible = true;
+                        }
+                        connect.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+            }
+        }
+        private void loadData()
+        {
+            SqlConnection connect = new SqlConnection(connectionString);
+            string query = "select user_ID as 'ID',SVC_No as 'Service No',Rank,Name,Post,Role from users";
+            SqlCommand command = new SqlCommand(query, connect);
+            SqlDataAdapter adapt = new SqlDataAdapter(command);
+            DataTable tbl = new DataTable();
+            adapt.Fill(tbl);
+            dataGridView1.DataSource = tbl;
+        }
+        int id=0;//Variable to hold the id of record to be updated or deleted
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtService.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtRank.Text= dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            txtName.Text= dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtPost.Text= dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtRole.Text= dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (id != 0)
+            {
+                string query="update users set SVC_No=@param,Rank=@param1,Name=@param2,Post=@param3,Role=@param4 where user_ID=@param5";
+                using(SqlConnection connect=new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@param", txtService.Text);
+                    command.Parameters.AddWithValue("@param1", txtRank.Text);
+                    command.Parameters.AddWithValue("@param2", txtName.Text);
+                    command.Parameters.AddWithValue("@param3", txtPost.Text);
+                    command.Parameters.AddWithValue("@param4", txtRole.Text);
+                    command.Parameters.AddWithValue("@param5", id);
+                    try
+                    {
+                        connect.Open();
+                        int i = command.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            MessageBox.Show("Record Updated Successfully");
+                            loadData();
+                            dataGridView1.Visible = true;
+                        }
+                        connect.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                id = 0;
+            }
+            else
+            {
+                MessageBox.Show("Select a record by double clicking the Datagrid", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (id != 0)
+            {
+                string query = "delete users where user_ID=@param";
+                using (SqlConnection connect=new SqlConnection(connectionString))
+                {
+                    SqlCommand command = new SqlCommand(query, connect);
+                    command.Parameters.AddWithValue("@param", id);
+                    try
+                    {
+                        connect.Open();
+                        int i = command.ExecuteNonQuery();
+                        if (i > 0)
+                        {
+                            MessageBox.Show("Record Deleted Successfully");
+                            loadData();
+                            dataGridView1.Visible = true;
+                        }
+                        connect.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                id = 0;
+            }
+            else
+            {
+                MessageBox.Show("Select a record by double clicking the Datagrid", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
